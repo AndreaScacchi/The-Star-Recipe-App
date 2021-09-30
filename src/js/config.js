8a81fefdff813164ec87d161223924b668c47de4
Andrea Scacchi <andreascacchi10@gmail.com>
2021-09-30 11:34:22 +0200

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+//export const KEY = "966aefab-a2d3-4053-9ab5-ed46b5580153";
+export const KEY = "083cb2a1-795b-4b8b-afb4-1dc3f868b6d2";
+export const MODAL_CLOSE_SEC = 2.5;
